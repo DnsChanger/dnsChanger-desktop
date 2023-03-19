11a64edecb88d5e300e95b9df882557400195f92
sajjadmrx <sajjadzibaee102@gmail.com>
2023-03-19 22:57:54 +0330

    untrack react router

---
+++ src/renderer/pages/setting.page.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from '../Wrappers/pages.wrapper';
+export function SettingPage() {
+
+    return (
+        <PageWrapper>
+            <div className='hero'>
+                <h1>تنظیمات</h1>
+            </div>
+        </PageWrapper>
+    )
+}
