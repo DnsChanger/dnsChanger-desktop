f1af8581720cee673774cea14b71f67e71550567
kehiy <kehiiiiya@gmail.com>
2023-05-23 09:41:00 -0700

    add russian config

---
+++ src/i18n/ru/index.ts
@@ -0,0 +1,57 @@
+import { BaseTranslation } from "../i18n-types";
+
+const ru: BaseTranslation = {
+  pages: {
+    home: {
+      homeTitle: "Лучшее снятие санкций",
+      connectedHTML: "Вы подключены к <u>{currentActive}</u> ",
+      connected: "Вы подключены к {currentActive}",
+      disconnected: "Прервано",
+      unknownServer: "Вы подключены к неизвестному серверу",
+    },
+    settings: {
+      title: "Настройки",
+      autoRunningTitle: "Автоматическое выполнение программы при включении системы",
+      langChanger: "Изменить язык",
+      themeChanger: "менять тему",
+    },
+    addCustomDns: {
+      NameOfServer: "имя сервера",
+      serverAddr: "адрес сервера",
+    },
+  },
+  themeChanger: {
+    dark: "темный",
+    light: "свет",
+  },
+  buttons: {
+    update: "список обновлений",
+    favDnsServer: "добавить собственный (DNS) сервер",
+    add: "добавлять",
+    flushDns: "очистить (Flush)",
+    ping: "серверы пингуются",
+  },
+  dialogs: {
+    fetching_data_from_repo: "получение данных из репозитория",
+    added_server: "{serverName} сервер успешно добавлен",
+    removed_server: "{serverName} сервер успешно удален",
+    flush_successful: "очистка успешно завершена",
+    flush_failure: "очистка не удалась",
+  },
+  errors: {
+    error_fetching_data: "Ошибка при получении данных от {target}",
+  },
+  connecting: "подключение...",
+  disconnecting: "отключение...",
+  waiting: "пожалуйста, подождите...",
+  successful: "успешный",
+  help_connect: "нажмите, чтобы подключиться",
+  help_disconnect: "нажмите, чтобы отключить",
+  validator: {
+    invalid_dns1: "DNS-значение 1 недопустимо.",
+    invalid_dns2: "DNS-значение 2 недопустимо.",
+    dns1_dns2_duplicates: "Значения DNS 1 и DNS 2 не должны совпадать.",
+  },
+};
+
+export default ru;
