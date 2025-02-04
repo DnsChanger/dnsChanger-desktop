dc5ef9de18829f56b7d5e57a63060a67e1c82a18
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-04 16:52:38 +0330

    refactor: improve notification component layout and add duration for toast messages

--- src/renderer/notifications/appNotif.tsx
+++ src/renderer/notifications/appNotif.tsx
@@ -21,26 +21,19 @@ export function appNotif(
 								<BiErrorAlt size={50} className={'text-red-500'} />
 							)}
 						</div>
-						<div className="ml-3 flex-1">
+						<div className="flex-1 ml-3">
 							<p className="text-sm font-medium ">{title}</p>
-							<p className="mt-1 text-sm dark:text-gray-500 text-gray-600">
+							<p className="mt-1 text-sm text-gray-600 dark:text-gray-500">
 								{msg}
 							</p>
 						</div>
 					</div>
 				</div>
-				<div className="flex border-l border-gray-200">
-					<button
-						onClick={() => toast.dismiss(t.id)}
-						className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-					>
-						Close
-					</button>
-				</div>
 			</div>
 		),
 		{
 			position: 'top-center',
+			duration: 200,
 		},
 	)
 }
